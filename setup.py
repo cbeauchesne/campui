1d9070b69b25a4e9b51681dcc93c6930da2eb316
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2017-04-03 13:37:21 +0200

    clean

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.txt')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
+
 
 requires = [
     'django==1.10.6',
@@ -23,7 +22,7 @@ setup(
     name='campui',
     version='0.0',
     description='campui',
-    long_description=README + '\n\n' + CHANGES,
+    long_description=README,
     classifiers=[
         'Programming Language :: Python',
         'Framework :: Django',
