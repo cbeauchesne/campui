f75bcb0301861d8e19e4adc4d42d808377159314
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2017-04-11 01:25:07 +0200

    fat marge

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+
+(function () {
+    angular.module('campui', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+        'ui.grid',                      // ui-grid
+        'ui.grid.autoResize',
+        'ngCookies',
+        'ngResource',
+        'angular.filter',
+        'ngSanitize',
+        'gettext',
+      //  'ngMap',
+        'tmh.dynamicLocale',
+        'slickCarousel',
+        'ngPhotoswipe',
+    ])
+})();
