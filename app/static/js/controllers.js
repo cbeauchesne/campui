bb3db195f98dc201f85d5c31d5e3e1a058368e63
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2017-04-15 17:26:25 +0200

    for #30

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -152,6 +152,12 @@ app.controller('searchController',['$scope','c2c','$state','searchData',function
     }
 }]);
 
+/*
 app.controller('forumController',['$scope','c2c',function($scope, c2c){
-    $scope.latest_topics = c2c.forum.latest_topics.get(function (data){console.log(data)})
+
+    $scope.latest_topics = c2c.forum.latest_topics.get(
+        function (data){console.log("s", data)},
+        function (data){console.log("e", data)}
+        )
 }]);
+*/
