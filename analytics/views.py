dd87a321d830cc47bcc4e70fed2833c61d81c72c
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2017-05-19 09:01:02 +0200

    Update views.py

--- analytics/views.py
+++ analytics/views.py
@@ -8,7 +8,7 @@ class AnalyticView(View):
         referer = request.META.get('HTTP_REFERER').split("/")
 
         domain = referer[2] if len(referer) >= 3 else request.META.get('HTTP_REFERER')
-        page_state = referer[3] if len(referer) >= 4 else ""
+        page_state = referer[3].split("?")[0] if len(referer) >= 4 else ""
 
         ps_object, _ = PageState.objects.get_or_create(name=page_state)
         domain_object, _ = Domain.objects.get_or_create(name=domain)
